702c1848af14b35252c8ef1e597a8a97cbcdfe78
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-04-25 19:27:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breakawayio::Application.config.secret_token = '680e7a5836d489507488f763b2403fdefeacbfc7c9550830bc7ff09457073b49ab5e71051980b9e581dcc9f885e17fec719545594142ec36589aa1406dd0d88e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Breakawayio::Application.config.session_store :cookie_store, key: '_breakawayio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Breakawayio::Application.config.session_store :active_record_store
